3c21463d554652e44a6b7261f7340b6246990627
Steven McVey <smcvey1210@gmail.com>
2023-11-21 13:20:04 -0500

    create main components

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <p>Header links</p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <h1>Nav Bar</h1>
+      <p>Nav bar links</p>
+    </div>
+  );
+}
+
+export default NavBar;
