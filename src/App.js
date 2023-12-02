82eda234b2a1fb9f99714801a00d761159e82174
Steven McVey <smcvey1210@gmail.com>
2023-12-02 12:25:48 -0500

    update image alt properties and remove unused useNavigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Projects from './Projects';
 import Home from './Home';
 import NavBar from './NavBar';
