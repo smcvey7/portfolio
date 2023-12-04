c1f4c6a3464c11d098c4912efdebb2c4897fedde
Steven McVey <smcvey1210@gmail.com>
2023-12-04 17:01:49 -0500

    update ProjectTile content and styling

--- src/ProjectTile.js
+++ src/ProjectTile.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function ProjectTile({name, description, image}){
-  console.log(name, description, image)
+function ProjectTile({name, description, image, github, demo, deployed}){
   return(
-    <div className="col">
-      <h2>{name}</h2>
+    <div className="col column-30">
+      <strong>{name}</strong><br/>
+      <div className="d-flex justify-content-around">
+        <a href={github}>Github</a>
+        <a href={demo}>Demo</a>
+        {deployed ? <a href={deployed}>Deployed</a> : null}
+
+      </div>
       <img src={image} alt={name} />
       <p>{description}</p>
     </div>
--- src/Projects.js
+++ src/Projects.js
@@ -5,25 +5,34 @@ function Projects() {
 
   const projectList = [
     {
-      name: "Test",
-      description: "This is the first project",
-      image: "https://via.placeholder.com/150"
+      name: "Chinese Character Test",
+      description: "Created using React and Rails, this test allows Chinese teachers and researchers to assess students' character recognition ability and to track information across classes over time.",
+      image: "https://via.placeholder.com/150",
+      github: "https://github.com/smcvey7/chinese-character-app",
+      demo: "https://www.youtube.com/watch?v=rQH2PUf91zA",
+      deployed: "https://www.chinesecharactertest.com/"
     },
     {
       name: "Scamps",
-      description: "This is the second project",
-      image: "https://via.placeholder.com/150"
+      description: "An application created with React and Rails to help parents sign their children up for local Summer camps",
+      image: "https://via.placeholder.com/150",
+      github: "https://github.com/smcvey7/scamps",
+      demo: "https://www.youtube.com/watch?v=uwol7GE5Ffo",
+      deployed: ""
     },
     {
       name: "Flashcards",
-      description: "This is the third project",
-      image: "https://via.placeholder.com/150"
+      description: "My first ever coding project; this app was primarily created using JavaScript. In this application, users create an account and then are able to create custom study lists simply by submitting a list of chinese characters. The app finds the pinyin romanization for the characters and creates flashcards with multiple choice options to test the user's knowledge. It keeps track of points earned by the user and displays top users in a leaderboard.",
+      image: "https://via.placeholder.com/150",
+      github: "https://github.com/smcvey7/phase-1-project-final",
+      demo: "https://www.youtube.com/watch?v=UEvHHccliTI",
+      deployed: ""
     }
   
   ]
 
   const projectTiles = projectList.map((project) => {
-    return <ProjectTile key={project.name} name={project.name} description={project.description} image={project.image} />
+    return <ProjectTile key={project.name} name={project.name} description={project.description} image={project.image} github={project.github} demo={project.demo} deployed={project.deployed} />
   })
 
 console.log(projectTiles)
@@ -36,7 +45,7 @@ console.log(projectTiles)
           <p>Projects page body content</p>
         </div>
       </div>
-      <div className="row" >
+      <div className="row align-items-start" >
         {projectTiles}
       </div>
     </div>
