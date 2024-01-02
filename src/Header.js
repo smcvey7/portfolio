b4311d37030b0cb6f5c5b75bf265b9d353240d48
Steven McVey <steven@stevenmcvey.com>
2024-01-02 08:15:43 -0800

    update Header and NavBar styling

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
   return (
     <div className="d-flex flex-horizontal justify-content-between mt-2 align-items-center">
       <div className="d-flex flex-horizontal align-items-center" >
         <img alt="Steven McVey" className="calligraphy" src="./img/calligraphy.png" />
-        <h1>Steven McVey</h1>
+        <div style={{position: "relative", top: "-5px"}} className="text-start">
+          <h1 style={{marginBottom: "0px"}}>Steven McVey</h1>
+          <em>Software Developer</em>
+        </div>
       </div>
       <div>
         <a href="https://www.linkedin.com/in/stevenlmcvey/" ><img alt="LinkedIn icon" className="link-icon" src="./img/linkedin-icon.png" /></a>
@@ -15,5 +19,10 @@ function Header() {
     </div>
   );
 }
+
+const StyledEm = styled("em")`
+  font-size: 1.5rem;
+  margin-top: 0px;
+`;
 
 export default Header;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import styled from "styled-components";
 
 function NavBar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary justify-content-around">
+    <StyledNavBar class="navbar navbar-expand-lg bg-body-tertiary justify-content-around">
+      <div style={{height: "100%"}} className="d-flex flex-horizontal justify-content-around align-items-center">
       <Link
         to="/"
-        className="btn btn-lg custom-button"
+        className="btn btn-lg custom-button navText"
         role="button"
       >
         Home
@@ -15,13 +16,38 @@ function NavBar() {
 
       <Link
         to="/projects"
-        className="btn btn-lg custom-button"
+        className="btn btn-lg custom-button navText"
         role="button"
       >
         Projects
       </Link>
-    </nav>
+
+      <Link
+        to="/cv"
+        className="btn btn-lg custom-button navText"
+        role="button"
+      >
+        CV
+      </Link>
+
+      <Link
+        to="/contact"
+        className="btn btn-lg custom-button navText"
+        role="button"
+      >
+        Hire Me
+      </Link>
+      </div>
+      
+    </StyledNavBar>
   );
 }
+
+const StyledNavBar = styled("div")`
+  height: 40px;
+  background-color: #5d7195;
+  color: #ffffff;
+  vertical-align: middle;
+`;
 
 export default NavBar;
