598f79a89a2547ac668bdd7216c9a6770563f79a
Steven McVey <steven@stevenmcvey.com>
2024-01-02 08:15:04 -0800

    update Footer styling

--- src/Footer.js
+++ src/Footer.js
@@ -4,22 +4,22 @@ import styled from "styled-components";
 function Footer() {
   return (
     <StyledFooter >
-      <h1>Footer</h1>
-      <p>Footer content</p>
+      <a href="mailto:steven@stevenmcvey.com" >steven@stevenmcvey.com</a>
     </StyledFooter>
   );
 }
 
 const StyledFooter = styled('div')`
 margin-top: 50px;
-margin-bottom: 0px
+margin-bottom: 0px;
 bottom: 0;
 width: 100%;
 background-color: #293241;
 color: #FFFFFF;
 font-size: 20px;
-text-align: center;
+text-align: left;
 padding-top: 20px;
+min-height: calc(100% - 20px);
 ` 
 
 export default Footer;
