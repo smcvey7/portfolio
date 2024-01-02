38d43f37bdafb535de6694a2b5d841d3830a4940
Steven McVey <steven@stevenmcvey.com>
2024-01-02 08:14:02 -0800

    add photo/update styling in Home

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,35 @@
 import React from 'react';
-
+import styled from 'styled-components';
 function Home(){
   return(
-    <div>
-      <h1>Home</h1>
-      <p>Home page body content</p>
-    </div>
+    <StyledHome className='d-flex flex-horizontal justify-content-between'>
+      <img alt='profile image' style={{width: "40%"}} src="./img/profile-photo.JPG"/>
+      <StyledHomeContent>
+        <em>My interests...</em><br/>
+        <strong>Technology</strong><br/>
+        <p>something</p>
+        <strong>Language</strong><br/>
+        <p>something</p>
+        <strong>Education</strong>
+        <p>something</p>
+
+
+      </StyledHomeContent>
+    </StyledHome>
   )
 }
+
+const StyledHome = styled('div')`
+  margin: 10px;
+  min-width: 400px;
+  padding: 15px;
+  background-color: #FFFFFF;
+  padding-top: 50px;
+`
+const StyledHomeContent = styled('div')`
+  width: 50%;
+  min-height: 300px;
+  border: none;
+`
 
 export default Home;
