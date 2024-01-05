71f6b3963111f5ac3330c30e3ece03875e282760
Steven McVey <steven@stevenmcvey.com>
2024-01-04 22:12:50 -0800

    add cv download and embed into page

--- src/CV.js
+++ src/CV.js
@@ -4,6 +4,9 @@ function CV() {
   return(
     <div>
       <h1>CV</h1>
+      <a href="./doc/Steven_McVey_Resume.pdf" download>Download CV</a>
+      <embed src="./doc/Steven_McVey_Resume.pdf" width="100%" height="800px" />
+
     </div>
   )
 }
