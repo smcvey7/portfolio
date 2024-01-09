4719d9b73d09238bf93227ed266777c79de3e394
Steven McVey <steven@stevenmcvey.com>
2024-01-09 12:47:55 -0800

    add contact form using actionforms

--- src/Contact.js
+++ src/Contact.js
@@ -4,6 +4,22 @@ function Contact() {
   return (
     <div>
       <h1>Contact Me</h1>
+      <form action="https://www.actionforms.io/e/r/portfolio-contact-form"
+    method="post">
+        <div>
+          <label for="name">Name</label>
+          <input type="text" name="name" />
+        </div>
+          <div>
+            <label for="email">Email</label>
+            <input type="email" email="email" />
+          </div>
+        <div>
+          <label for="message">Message</label>
+          <textarea name="message"></textarea>
+        </div>
+        <button type="submit">Send form</button>
+      </form>
     </div>
   )
 }
