f08ad5c0e6c9e111a6e36a74607f0ef95e3d8961
Steven McVey <steven@stevenmcvey.com>
2024-01-26 09:49:43 -0500

    show maintenance page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Projects from './Projects';
-import Home from './Home';
-import NavBar from './NavBar';
-import Header from './Header';
-import Footer from './Footer';
+// import { Route, Routes } from 'react-router-dom';
+// import Projects from './Projects';
+// import Home from './Home';
+// import NavBar from './NavBar';
+// import Header from './Header';
+// import Footer from './Footer';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import CV from './CV';
-import Contact from './Contact';
-import Error from './Error';
+// import CV from './CV';
+// import Contact from './Contact';
+// import Error from './Error';
 import Maintenance from './Maintenance';
 
 function App() {
